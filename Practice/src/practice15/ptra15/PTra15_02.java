fe22d6261d2f3eba8905fe85e8e7beca1fa0bf4b
KoukiSatoh <iskifg0124@gmail.com>
2018-01-22 12:38:28 +0900

    PTra15_02まで完了

--- Practice/src/practice15/ptra15/PTra15_02.java
+++ Practice/src/practice15/ptra15/PTra15_02.java
@@ -1,10 +1,11 @@
 /*
- * PTra15_02.java
+  * PTra15_02.java
  *   作成	LIKEIT	2017
  *------------------------------------------------------------
  * Copyright(c) Rhizome Inc. All Rights Reserved.
  */
 package practice15.ptra15;
+
 
 import practice15.common.Java2Month;
 import practice15.common.Room;
@@ -12,12 +13,22 @@ import practice15.common.Room;
 public class PTra15_02 {
 	public static void main(String[] args) {
 
-		Room room = new Java2Month();
+		Room room = new Java2Month();//Java2Monthのインスタンスを作成
+
 
 		// roomに格納されたJava2MonthインスタンスのgetCurriculumメソッドを呼び出してください
+		Java2Month j2m = (Java2Month)room;//roomをJava2MonthでキャストしてgetCurriculumメソッドを呼び出せるようにした
 
 
 		// 取得したカリキュラム情報を全て出力してください
+
+		String[] curriculumList = j2m.getCurriculum(); //String型の配列Java2に.getCurriculumを代入？
+
+		for(String curriculum :curriculumList) {
+			System.out.println(curriculum);
+		}
+
+//わからんので、一旦後回しで先に進む。
 
 	}
 }
